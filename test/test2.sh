e9b2924a46395238678ce82ae5e8a9eb974ad38e
ismaelGTI <ismaelmarin11@uma.es>
2025-04-03 10:20:56 +0200

    OK!

--- test/test2.sh
+++ test/test2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Running test2.sh"
+echo "Soy el test 2"
 echo "This is a test script."
 echo "This script is located in the test directory."
 echo "The current directory is: $(pwd)"
--- test/test3.sh
+++ test/test3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Running test3.sh"
+echo "Soy el test 3"
 echo "This is a test script."
 echo "This script is located in the test directory."
 echo "The current directory is: $(pwd)"
